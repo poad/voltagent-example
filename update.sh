0097e86725bdc80cab813428604e7a5601595064
Kenji Saito <ken-yo@mbr.nifty.com>
2026-05-27 20:27:05 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -22,7 +22,7 @@ fi
 echo ""
 pwd
 
-if ! (npx -y pnpm@latest self-update latest-11 && pnpm install -r && pnpm up -r && pnpm audit --fix override && pnpm up -r && pnpm -r lint-fix && pnpm -r build); then
+if ! (disable-checkout-persist-credentials && pinact run -u && pnx pnpm@latest self-update latest-11 && pnpm install -r && pnpm up -r && pnpm audit --fix override && pnpm up -r && pnpm -r lint-fix && pnpm -r build); then
   cd "${CUR}" || exit
   exit 1
 fi
